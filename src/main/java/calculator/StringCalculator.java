93cc3fa55d6bf9d8661420e89afbd6230a6620ff
Prathamesh Raut <praut366@gmail.com>
2021-10-17 23:14:00 +0530

    String add functionality for multiple numbers with comma delimiter

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -2,8 +2,23 @@ package calculator;
 
 class StringCalculator {
 
-    public int add(String input) {
-        return 0;
-    }
+	public int add(String input) {
+
+		// Commit 1 starts here  answer for Question 1 and 2
+		if (input.isEmpty()) {
+			return 0;
+		}
+		String[] numbers = input.split(",");
+		if (numbers.length == 1) {
+			return Integer.parseInt(numbers[0]);
+		}
+		int sum = 0;
+		for (String string : numbers) {
+			sum = sum + Integer.parseInt(string);
+		}
+		return sum;
+
+		// Commit 1 ends here
+	}
 
 }
